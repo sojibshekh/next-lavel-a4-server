cda31da6a7efb3f28927c2fb204f35ccf4afe5c4
Sojib Sheikh <sojib02018@gmail.com>
2025-07-03 22:49:16 +0600

    live link here

--- dist/app.js
+++ dist/app.js
@@ -9,7 +9,7 @@ const borrow_controler_1 = require("./app/controllers/borrow.controler");
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: ['http://localhost:5173', 'live-deploy-url']
+    origin: ['https://next-lavel-a4-backend.vercel.app/']
 }));
 app.use(express_1.default.json());
 app.use("/api", books_controler_1.bookRoutes);
